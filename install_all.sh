cc0f273e66cba990502039f1513e269b172602d0
zyn <43765256+zyn23@users.noreply.github.com>
2025-08-24 13:45:30 +0100

    Create install_all.sh
    TODO add more stuff that I use and automate some things

---
+++ install_all.sh
@@ -0,0 +1,2 @@
+sudo apt update && sudo apt upgrade 
+sudo apt install kitty pipewire curl wget thunar rofi sassc eza rofi build-essential btop l3afpad micro mpv mupdf mupdf-tools tealdeer bat nsxiv fooyin ffmpeg yt-dlp clementine vlc gimp obs-studio steam flameshot

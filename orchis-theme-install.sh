76f2509b0356ee562d62cb7a122e706160323de3
zyn <43765256+zyn23@users.noreply.github.com>
2025-09-27 16:52:58 +0100

    Update and rename orchis-theme-install.sh to theming_install.sh

--- orchis-theme-install.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-##Download Orchis-theme
-git clone https://github.com/vinceliuice/Orchis-theme.git
-cd Orchis-theme
-## Install with my tweaks and choices
-./install.sh -t grey purple red -c dark -s compact --tweaks macos
---
+++ theming_install.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+##Download Orchis-theme
+git clone https://github.com/vinceliuice/Orchis-theme.git
+cd Orchis-theme
+## Install with my tweaks and choices
+./install.sh -t grey purple red -c dark -s compact --tweaks macos
+
+cd $HOME
+
+##ROFI THEMES INSTALATION - MINIMAL
+#NEED TO CHANGE COLORSCHEME LATER
+git clone https://github.com/Murzchnvok/rofi-collection
+cd rofi-collection/
+sudo cp -r minimal/minimal.rasi /usr/share/rofi/themes/
+
+cd $HOME
+
+##INSTALL NOVA OS ICON PACK
+git clone https://github.com/NicklasVraa/NovaOS-nord-Icons.git
+cd NovaOS-nord-Icons/
+cp -r NovaOS-nord/ ~/.icons/
+
